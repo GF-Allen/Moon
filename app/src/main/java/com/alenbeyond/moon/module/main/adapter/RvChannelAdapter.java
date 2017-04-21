4b1dbecb3a0ced8cae5ced5797070e9e9427aabf
xiqingliu <liuxiqing505@gmail.com>
2017-04-21 14:30:34 +0800

    no message

--- app/src/main/java/com/alenbeyond/moon/module/main/adapter/RvChannelAdapter.java
+++ app/src/main/java/com/alenbeyond/moon/module/main/adapter/RvChannelAdapter.java
@@ -4,11 +4,13 @@ import android.content.Context;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.alenbeyond.moon.R;
 import com.alenbeyond.moon.base.adapter.MoonBaseRecyclerViewAdapter;
 import com.alenbeyond.moon.model.bean.News;
+import com.alenbeyond.moon.utils.glide.GlideUtils;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
@@ -32,12 +34,24 @@ public class RvChannelAdapter extends MoonBaseRecyclerViewAdapter<News.NewsListB
     @Override
     protected void doOnBindViewHolder(ViewHolder holder, int position) {
         holder.tvTitle.setText(objectList.get(position).getNewsBean().getTitle());
+        holder.tvDes.setText(objectList.get(position).getNewsBean().getDigest());
+        holder.tvOwner.setText(objectList.get(position).getAuthorBean().getName());
+        GlideUtils.getInstance().loadCircleImage(mContext, holder.ivOwner, objectList.get(position).getAuthorBean().getHeadUrl());
+        GlideUtils.getInstance().loadImage(mContext, holder.ivPic, objectList.get(position).getNewsBean().getCoverUrl(), true);
     }
 
     static class ViewHolder extends RecyclerView.ViewHolder {
 
         @BindView(R.id.tv_title)
         TextView tvTitle;
+        @BindView(R.id.tv_des)
+        TextView tvDes;
+        @BindView(R.id.tv_owner)
+        TextView tvOwner;
+        @BindView(R.id.iv_owner)
+        ImageView ivOwner;
+        @BindView(R.id.iv_pic)
+        ImageView ivPic;
 
         ViewHolder(View itemView) {
             super(itemView);
